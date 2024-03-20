fe395b5bc7f5e5672de1693cb2764781c9e2c33b
michael <mchege78@gmail.com>
2024-03-20 13:01:02 +0300

    Added charts

--- src/components/chart.tsx
+++ src/components/chart.tsx
@@ -1,7 +1,29 @@
-import { FC } from "react";
+import React from "react";
+import { Line } from "@ant-design/charts";
 
-const Chart: FC = () => {
-    return(
-        <div></div>
-    )
-}
+const Chart: React.FC = () => {
+  const data = [
+    { year: "1991", value: 3 },
+    { year: "1992", value: 4 },
+    { year: "1993", value: 3.5 },
+    { year: "1994", value: 5 },
+    { year: "1995", value: 4.9 },
+    { year: "1996", value: 6 },
+    { year: "1997", value: 7 },
+    { year: "1998", value: 9 },
+    { year: "1999", value: 13 },
+  ];
+
+  const config = {
+    data,
+    xField: "year",
+    yField: "value",
+  };
+
+  return (
+    <div className="chart">
+      <Line height={250} {...config} />
+    </div>
+  );
+};
+export default Chart;
--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,18 +1,25 @@
-import { FC } from "react"
-import { Layout} from "antd"
-import Header from "./Header"
-import Highlight from "./hightlight"
+import { FC } from "react";
+import { Layout } from "antd";
+import Header from "./Header";
+import Highlight from "./hightlight";
+import Chart from "./chart";
 
-const Main:FC = () => {
-
-    return (
-        <Layout style={{minHeight: '100vh', padding: '4rem', display: 'flex', flexDirection:'column', gap: '2rem'}}>
-
-            <Header />
-            <Highlight />
-        </Layout>
-    )
-
-}
+const Main: FC = () => {
+  return (
+    <Layout
+      style={{
+        height: "100vh",
+        padding: "4rem",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+      }}
+    >
+      <Header />
+      <Highlight />
+      <Chart />
+    </Layout>
+  );
+};
 
 export default Main;
